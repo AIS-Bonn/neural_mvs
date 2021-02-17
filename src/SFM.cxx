934c4d6df400726888d30e6cc55b5a3ab6e7dfdc
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2021-02-17 09:45:48 +0000

    Trying to do direcly bundle adjustmenet instead of triangulation in order to get the 3D points. So I switched to ceres instead of libmv. It compiles but the results looks bad
    Change-Id: I586ff57fe92390c55c4ac938bbb512957b37cf7e

--- src/SFM.cxx
+++ src/SFM.cxx
@@ -31,7 +31,7 @@ using namespace configuru;
 //opencv 
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
-#include "opencv2/sfm/triangulation.hpp"
+// #include "opencv2/sfm/triangulation.hpp"
 #include <opencv2/core/eigen.hpp>
 
 
@@ -39,10 +39,93 @@ using namespace configuru;
 #include <boost/filesystem.hpp>
 namespace fs = boost::filesystem;
 
+//ceres 
+#include "ceres/ceres.h"
+#include "ceres/rotation.h"
+using namespace ceres;
+
+
+//in this contructor we put the things that are not going to be optimized
+struct ReprojectionError {
+  ReprojectionError(Eigen::Vector2d point_observed, Eigen::Matrix3d K, Eigen::Affine3d tf_cam_world)
+      : m_point_observed(point_observed), m_K(K), m_tf_cam_world(tf_cam_world)  {}
+
+
+  //things that will be optimized go here
+  template <typename T>
+  bool operator()(
+                  const T* const point,
+                  T* residuals) const {
+
+
+    //typedefs
+    typedef Eigen::Matrix<T, 3, 1> Vec3;
+    typedef Eigen::Transform<T, 3, Eigen::Affine> Affine3d;
+    // typedef Eigen::Quaternion<T> Quaterniond;
+    // typedef Eigen::Matrix<T, 4, 3> Mat4x3;
+    typedef Eigen::Matrix<T, 3, 3> Mat3x3;
+
+
+    //we need these things as jet object
+    Affine3d tf_cam_world;
+    // tf_cam_world.linear()=m_tf_cam_world.linear();
+    // tf_cam_world.translation()=m_tf_cam_world.translation();
+    tf_cam_world=m_tf_cam_world.cast<T>();
+
+
+    //K matrix because we need it as Jet object
+    Mat3x3 K;
+    K=m_K.cast<T>();
+    // K(0,0)=T(m_K(0,0));
+    // K(0,1)=T(m_K(0,1));
+    // K(0,2)=T(m_K(0,2));
+    // //
+    // K(1,0)=T(m_K(1,0));
+    // K(1,1)=T(m_K(1,1));
+    // K(1,2)=T(m_K(1,2));
+    // //
+    // K(2,0)=T(m_K(2,0));
+    // K(2,1)=T(m_K(2,1));
+    // K(2,2)=T(m_K(2,2));
+
+    //make the point in 3D
+    Vec3 point_world; 
+    point_world.x()=point[0];
+    point_world.y()=point[1];
+    point_world.z()=point[2];
+
+    //project the 3d into 2D 
+    Vec3 point_cam=tf_cam_world*point_world;
+    point_cam.x()/=point_cam.z();
+    point_cam.y()/=point_cam.z();
+    point_cam.z()=T(1.0);
+    Vec3 point_2d = K*point_cam;
+
+    T error_x= T(m_point_observed.x()) - point_2d.x();
+    T error_y= T(m_point_observed.y()) - point_2d.y();
+
+    residuals[0] = error_x;
+    residuals[1] = error_y;
+
+
+    return true;
+  }
+
+   // Factory to hide the construction of the CostFunction object from
+   // the client code.
+   static ceres::CostFunction* Create( Eigen::Vector2d point_observed, Eigen::Matrix3d K, Eigen::Affine3d tf_cam_world) {
+     return (new ceres::AutoDiffCostFunction<ReprojectionError, 2, 3>( //the first number is the number of residuals we output,  the rest of the number is the dimensions that we optimize of the inputs to the operator() 
+                 new ReprojectionError( point_observed, K, tf_cam_world )));
+   }
+ 
+
+    Eigen::Vector2d m_point_observed;
+    Eigen::Matrix3d m_K;
+    Eigen::Affine3d m_tf_cam_world;
+};
 
 
 
-//CPU code that calls the kernels
 SFM::SFM()
     {
 
@@ -232,69 +315,132 @@ easy_pbr::MeshSharedPtr SFM::compute_3D_keypoints_from_frames(const easy_pbr::Fr
     VLOG(1) << "traingulate";
 
 
-    //attempt 2
-    cv::Mat cam_0_points(2, matches.size(), CV_64FC1);
-    cv::Mat cam_1_points(2, matches.size(), CV_64FC1);
+    // //attempt 2
+    // cv::Mat cam_0_points(2, matches.size(), CV_64FC1);
+    // cv::Mat cam_1_points(2, matches.size(), CV_64FC1);
+    // for(size_t m_idx=0; m_idx<matches.size(); m_idx++){
+    //     cv::DMatch match=matches[m_idx];
+    //     int query_idx=match.queryIdx;
+    //     int target_idx=match.trainIdx;
+
+    //     cam_0_points.at<double>(0, m_idx)= query_keypoints[query_idx].pt.x;
+    //     cam_0_points.at<double>(1, m_idx)= query_keypoints[query_idx].pt.y;
+
+    //     cam_1_points.at<double>(0, m_idx)= target_keypoints[target_idx].pt.x;
+    //     cam_1_points.at<double>(1, m_idx)= target_keypoints[target_idx].pt.y;
+
+    // }
+    
+
+
+    // // VLOG(1) << points1Mat.rows << " x " << points1Mat.cols;
+    // // VLOG(1) << "cam_0_points " << cam_0_points;
+    // //matrices of each cam
+    // cv::Mat cam_0_matrix;
+    // cv::Mat cam_1_matrix;
+    // // Eigen::MatrixXd cam_0_matrix_eigen= ( frame_query.K * frame_query.tf_cam_world.matrix().block<3,4>(0,0) ).cast<double>();
+    // // Eigen::MatrixXd cam_1_matrix_eigen= ( frame_target.K * frame_target.tf_cam_world.matrix().block<3,4>(0,0) ).cast<double>();
+
+    // //with cma query being at identity
+    // Eigen::Affine3f tf_cam_query_target=  frame_query.tf_cam_world* frame_target.tf_cam_world.inverse(); //target to world and world to query
+    // Eigen::Affine3f id=tf_cam_query_target;
+    // id.setIdentity();
+    // Eigen::MatrixXd cam_0_matrix_eigen= ( frame_query.K * id.matrix().block<3,4>(0,0) ).cast<double>();
+    // Eigen::MatrixXd cam_1_matrix_eigen= ( frame_target.K * tf_cam_query_target.matrix().block<3,4>(0,0) ).cast<double>();
+    // cv::eigen2cv(cam_0_matrix_eigen, cam_0_matrix);
+    // cv::eigen2cv(cam_1_matrix_eigen, cam_1_matrix);
+    // // VLOG(1) << "cam eigen is " << cam_0_matrix;
+    // // VLOG(1) << "cam mat is " << cam_0_matrix;
+    // std::vector<cv::Mat> cam_matrices;
+    // cam_matrices.push_back(cam_0_matrix);
+    // cam_matrices.push_back(cam_1_matrix);
+
+    // // std::vector<std::vector<cv::Point2f>> points2d_for_each_frame;
+    // std::vector<cv::Mat> points2d_for_each_frame;
+    // points2d_for_each_frame.push_back(cam_0_points);
+    // points2d_for_each_frame.push_back(cam_1_points);
+    // //DEBUG 
+    // // points2d_for_each_frame.push_back(points1Mat);
+    // // points2d_for_each_frame.push_back(points1Mat);
+
+
+    // // cv::sfm::triangulatePoints(dummy, dummy, dummy);
+    // cv::Mat dummy;
+    // // std::vector<cv::Point3d> points_3d;
+    // cv::Mat points_3d;
+    // cv::sfm::triangulatePoints(points2d_for_each_frame, cam_matrices, points_3d);
+
+    // //put the points to V 
+    // int nr_points=points_3d.cols;
+    // mesh->V.resize( nr_points, 3 );
+    // for(int p_idx=0; p_idx<nr_points; p_idx++){
+    //     mesh->V(p_idx, 0) = points_3d.at<double>(0, p_idx);
+    //     mesh->V(p_idx, 1) = -points_3d.at<double>(1, p_idx);
+    //     mesh->V(p_idx, 2) = points_3d.at<double>(2, p_idx);
+    // }
+
+
+
+
+    //attempt 3 using ceres
+    ceres::Problem problem;
+
+    std::vector<double> points_3d( matches.size()*3, 0); 
+
+
     for(size_t m_idx=0; m_idx<matches.size(); m_idx++){
         cv::DMatch match=matches[m_idx];
         int query_idx=match.queryIdx;
         int target_idx=match.trainIdx;
 
-        cam_0_points.at<double>(0, m_idx)= query_keypoints[query_idx].pt.x;
-        cam_0_points.at<double>(1, m_idx)= query_keypoints[query_idx].pt.y;
+        Eigen::Vector2d query_observed;
+        query_observed << query_keypoints[query_idx].pt.x, query_keypoints[query_idx].pt.y;
 
-        cam_1_points.at<double>(0, m_idx)= target_keypoints[target_idx].pt.x;
-        cam_1_points.at<double>(1, m_idx)= target_keypoints[target_idx].pt.y;
+        Eigen::Vector2d target_observed;
+        target_observed <<  target_keypoints[target_idx].pt.x, target_keypoints[target_idx].pt.y;
 
+        ceres::CostFunction* cost_function_query = ReprojectionError::Create( query_observed, frame_query.K.cast<double>(), frame_query.tf_cam_world.cast<double>() );
+        ceres::CostFunction* cost_function_target = ReprojectionError::Create( target_observed, frame_target.K.cast<double>(), frame_target.tf_cam_world.cast<double>() );
+
+        problem.AddResidualBlock(cost_function_query,
+                               NULL /* squared loss */,
+                            // loss_function,
+                            points_3d.data()+m_idx*3
+                            );
+        problem.AddResidualBlock(cost_function_target,
+                               NULL /* squared loss */,
+                            // loss_function,
+                            points_3d.data()+m_idx*3
+                            );
+
+
+    }
+
+
+    //solve
+    ceres::Solver::Options options;
+    options.linear_solver_type = ceres::DENSE_SCHUR;
+    options.minimizer_progress_to_stdout = true;
+    options.max_num_iterations=400;
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.FullReport() << "\n";
+    VLOG_S(1) << summary.FullReport();
+
+
+     //put the points to V 
+    int nr_points=matches.size();
+    mesh->V.resize( nr_points, 3 );
+    for(int p_idx=0; p_idx<nr_points; p_idx++){
+        mesh->V(p_idx, 0) = points_3d[p_idx*3+0];
+        mesh->V(p_idx, 1) = -points_3d[p_idx*3+1];
+        mesh->V(p_idx, 2) = points_3d[p_idx*3+2];
     }
     
 
 
-    // VLOG(1) << points1Mat.rows << " x " << points1Mat.cols;
-    // VLOG(1) << "cam_0_points " << cam_0_points;
-    //matrices of each cam
-    cv::Mat cam_0_matrix;
-    cv::Mat cam_1_matrix;
-    // Eigen::MatrixXd cam_0_matrix_eigen= ( frame_query.K * frame_query.tf_cam_world.matrix().block<3,4>(0,0) ).cast<double>();
-    // Eigen::MatrixXd cam_1_matrix_eigen= ( frame_target.K * frame_target.tf_cam_world.matrix().block<3,4>(0,0) ).cast<double>();
-
-    //with cma query being at identity
-    Eigen::Affine3f tf_cam_query_target=  frame_query.tf_cam_world* frame_target.tf_cam_world.inverse(); //target to world and world to query
-    Eigen::Affine3f id=tf_cam_query_target;
-    id.setIdentity();
-    Eigen::MatrixXd cam_0_matrix_eigen= ( frame_query.K * id.matrix().block<3,4>(0,0) ).cast<double>();
-    Eigen::MatrixXd cam_1_matrix_eigen= ( frame_target.K * tf_cam_query_target.matrix().block<3,4>(0,0) ).cast<double>();
-    cv::eigen2cv(cam_0_matrix_eigen, cam_0_matrix);
-    cv::eigen2cv(cam_1_matrix_eigen, cam_1_matrix);
-    // VLOG(1) << "cam eigen is " << cam_0_matrix;
-    // VLOG(1) << "cam mat is " << cam_0_matrix;
-    std::vector<cv::Mat> cam_matrices;
-    cam_matrices.push_back(cam_0_matrix);
-    cam_matrices.push_back(cam_1_matrix);
-
-    // std::vector<std::vector<cv::Point2f>> points2d_for_each_frame;
-    std::vector<cv::Mat> points2d_for_each_frame;
-    points2d_for_each_frame.push_back(cam_0_points);
-    points2d_for_each_frame.push_back(cam_1_points);
-    //DEBUG 
-    // points2d_for_each_frame.push_back(points1Mat);
-    // points2d_for_each_frame.push_back(points1Mat);
 
 
-    // cv::sfm::triangulatePoints(dummy, dummy, dummy);
-    cv::Mat dummy;
-    // std::vector<cv::Point3d> points_3d;
-    cv::Mat points_3d;
-    cv::sfm::triangulatePoints(points2d_for_each_frame, cam_matrices, points_3d);
-
-    //put the points to V 
-    int nr_points=points_3d.cols;
-    mesh->V.resize( nr_points, 3 );
-    for(int p_idx=0; p_idx<nr_points; p_idx++){
-        mesh->V(p_idx, 0) = points_3d.at<double>(0, p_idx);
-        mesh->V(p_idx, 1) = -points_3d.at<double>(1, p_idx);
-        mesh->V(p_idx, 2) = points_3d.at<double>(2, p_idx);
-    }
 
 
 
