Can switch between the computation of close frame based on view angle or on delaunay, However the weights computation for delaunay seem wrong
Change-Id: I22de8fcefc82d6cf9dab7494ac4a356cc3c59b41

neural_mvs_py/neural_mvs/models.py
@@ -3758,7 +3758,7 @@ class DifferentiableRayMarcher(torch.nn.Module):
         self.out_layer = BlockNerf(activ=None, in_channels=self.lstm_hidden_size, out_channels=1,  bias=True ).cuda()
         # self.feature_computer= VolumetricFeature(in_channels=3, out_channels=64, nr_layers=2, hidden_size=64, use_dirs=False) 
         # self.feature_computer= VolumetricFeatureSiren(in_channels=3, out_channels=64, nr_layers=2, hidden_size=64, use_dirs=False) 
-        self.frame_weights_computer= FrameWeightComputer()
+        # self.frame_weights_computer= FrameWeightComputer()
         self.feature_aggregator= FeatureAgregator()
         self.slice_texture= SliceTextureModule()
         self.splat_texture= SplatTextureModule()
@@ -3780,7 +3780,7 @@ class DifferentiableRayMarcher(torch.nn.Module):
         self.nr_iters=10
 
       
-    def forward(self, frame, depth_min, frames_close, frames_features, novel=False):
+    def forward(self, frame, depth_min, frames_close, frames_features, weights, novel=False):
 
 
         if novel:
@@ -3808,7 +3808,7 @@ class DifferentiableRayMarcher(torch.nn.Module):
         signed_distances_for_marchlvl=[]
         states = [None]
 
-        weights=self.frame_weights_computer(frame, frames_close)
+        # weights=self.frame_weights_computer(frame, frames_close)
 
         for iter_nr in range(self.nr_iters):
             # print("iter is ", iter_nr)
@@ -5050,7 +5050,7 @@ class Net3_SRN(torch.nn.Module):
         # with torch.set_grad_enabled(False):
             # self.s_weight.fill_(0.5)
             # self.s_weight.fill_(10.0)
-        self.frame_weights_computer= FrameWeightComputer()
+        # self.frame_weights_computer= FrameWeightComputer()
         self.feature_aggregator= FeatureAgregator()
 
 
@@ -5068,7 +5068,7 @@ class Net3_SRN(torch.nn.Module):
 
 
       
-    def forward(self, frame, mesh, depth_min, depth_max, frames_close, novel=False):
+    def forward(self, frame, mesh, depth_min, depth_max, frames_close, weights, novel=False):
 
         TIME_START("unet_everything")
         # frames_features=[]
@@ -5092,7 +5092,7 @@ class Net3_SRN(torch.nn.Module):
         TIME_END("unet_everything")
 
         TIME_START("ray_march")
-        point3d, depth, points3d_for_marchlvl, signed_distances_for_marchlvl = self.ray_marcher(frame, depth_min, frames_close, frames_features, novel)
+        point3d, depth, points3d_for_marchlvl, signed_distances_for_marchlvl = self.ray_marcher(frame, depth_min, frames_close, frames_features, weights, novel)
         TIME_END("ray_march")
 
         # print("len points3d_for_marchlvl", len(points3d_for_marchlvl))
@@ -5161,14 +5161,14 @@ class Net3_SRN(torch.nn.Module):
           
 
         ##attempt 4 
-        weights=self.frame_weights_computer(frame, frames_close)
+        # weights=self.frame_weights_computer(frame, frames_close)
         img_features_aggregated= self.feature_aggregator(frame, frames_close, sliced_feat_batched, weights)
         std= img_features_aggregated[:, -16]
 
         # show the frames and with a line weight depending on the weight
         for i in range(len(frames_close)):
             frustum_mesh=frames_close[i].frame.create_frustum_mesh(0.02)
-            frustum_mesh.m_vis.m_line_width= (weights[i])*15
+            frustum_mesh.m_vis.m_line_width= (weights[i])*10
             frustum_mesh.m_vis.m_line_color=[0.0, 1.0, 0.0] #green
             Scene.show(frustum_mesh, "frustum_neighb_"+str(i) ) 
         

neural_mvs_py/train.py
@@ -82,7 +82,7 @@ def get_close_frames_barycentric(frame_py, all_frames_py_list, discard_same_idx,
 
     triangulated_mesh=SFM.compute_triangulation_stegreographic( frame_centers, sphere_center, sphere_radius )
 
-    face= SFM.compute_closest_triangle( frame_py.frame.pos_in_world(), triangulated_mesh )
+    face, weights= SFM.compute_closest_triangle( frame_py.frame.pos_in_world(), triangulated_mesh )
 
     #from the face get the vertices that we triangulated
     # print("frame idx ", frame_idx_0, frame_idx_1, frame_idx_2 )
@@ -96,7 +96,7 @@ def get_close_frames_barycentric(frame_py, all_frames_py_list, discard_same_idx,
             selected_frames.append(frame)
     
 
-    return selected_frames
+    return selected_frames, weights
 
 
 class FramePY():
@@ -336,6 +336,7 @@ def run():
     frame_centers, frame_idxs = frames_to_points(frames_train)
     sphere_center, sphere_radius=SFM.fit_sphere(frame_centers)
     print("sphere center and raidys ", sphere_center, " radius ", sphere_radius)
+    frame_weights_computer= FrameWeightComputer()
 
     # triangulated_mesh, sphere_center, sphere_radius=SFM.compute_triangulation(loader_train.get_all_frames())
 
@@ -402,11 +403,18 @@ def run():
 
                         # frames_close=loader_train.get_close_frames(frame, 2)
                         discard_same_idx=is_training # if we are training we don't select the frame with the same idx, if we are testing, even if they have the same idx there are from different sets ( test set and train set)
-                        # frames_close=get_close_frames(loader_train, frame, frames_train, 5, discard_same_idx) #the neighbour are only from the training set
-                        frames_close=get_close_frames_barycentric(frame, frames_train, discard_same_idx, sphere_center, sphere_radius)
-                        # print("frames_close", len(frames_close))
+
+                        #closenes here is defined in terms of A
+                        do_close_computation_with_delaunay=True
+                        if not do_close_computation_with_delaunay:
+                            frames_close=get_close_frames(loader_train, frame, frames_train, 5, discard_same_idx) #the neighbour are only from the training set
+                            weights= frame_weights_computer(frame, frames_close)
+                        else:
+                            frames_close, weights=get_close_frames_barycentric(frame, frames_train, discard_same_idx, sphere_center, sphere_radius)
+                            weights= torch.from_numpy(weights.copy()).to("cuda").float() 
+
                         TIME_START("forward")
-                        rgb_pred, depth_pred, signed_distances_for_marchlvl, std=model(frame, mesh_full, depth_min, depth_max, frames_close, novel=not phase.grad)
+                        rgb_pred, depth_pred, signed_distances_for_marchlvl, std=model(frame, mesh_full, depth_min, depth_max, frames_close, weights, novel=not phase.grad)
                         TIME_END("forward")
 
                      