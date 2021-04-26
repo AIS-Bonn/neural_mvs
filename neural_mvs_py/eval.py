e2c9b8754f767077375df41b2adefbfda65aad33
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2021-04-26 03:07:03 +0000

    Made the eval run correctly on the test set in case of DTU
    Change-Id: I1eacb2b95a808ee6b86a9fe5a3351a44e71c6a31

--- neural_mvs_py/eval.py
+++ neural_mvs_py/eval.py
@@ -197,13 +197,27 @@ def run():
             frame=FramePY(frame.frame)
 
 
-            discard_same_idx=False
+            # discard_same_idx=False
+            # do_close_computation_with_delaunay=True
+            # if not do_close_computation_with_delaunay:
+            #     frames_close=get_close_frames(loader_train, frame, frames_train, 5, discard_same_idx) #the neighbour are only from the training set
+            #     weights= frame_weights_computer(frame, frames_close)
+            # else:
+            #     frames_close, weights=get_close_frames_barycentric(frame, frames_train, discard_same_idx, sphere_center, sphere_radius)
+            #     weights= torch.from_numpy(weights.copy()).to("cuda").float() 
+
+            discard_same_idx= False
+            if isinstance(loader_train, DataLoaderShapeNetImg) or isinstance(loader_train, DataLoaderSRN) or isinstance(loader_train, DataLoaderDTU):
+                discard_same_idx=True
+            frames_to_consider_for_neighbourhood=frames_train
+            if isinstance(loader_train, DataLoaderShapeNetImg) or isinstance(loader_train, DataLoaderSRN) or isinstance(loader_train, DataLoaderDTU): #if it's these loader we cannot take the train frames for testing because they dont correspond to the same object
+                frames_to_consider_for_neighbourhood=frames_test
             do_close_computation_with_delaunay=True
             if not do_close_computation_with_delaunay:
-                frames_close=get_close_frames(loader_train, frame, frames_train, 5, discard_same_idx) #the neighbour are only from the training set
+                frames_close=get_close_frames(loader_train, frame, frames_to_consider_for_neighbourhood, 5, discard_same_idx) #the neighbour are only from the training set
                 weights= frame_weights_computer(frame, frames_close)
             else:
-                frames_close, weights=get_close_frames_barycentric(frame, frames_train, discard_same_idx, sphere_center, sphere_radius)
+                frames_close, weights=get_close_frames_barycentric(frame, frames_to_consider_for_neighbourhood, discard_same_idx, sphere_center, sphere_radius)
                 weights= torch.from_numpy(weights.copy()).to("cuda").float() 
 
             #load frames
