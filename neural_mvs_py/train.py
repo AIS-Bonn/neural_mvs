28e56b491cdd2d2906816961d1aa1e5dedb5f21f
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2021-06-21 15:04:45 +0000

    Configured for training LLFF on the servers
    Change-Id: I0ca9ff87b87d0ae215eb6683e83771254f81cc88

--- neural_mvs_py/train.py
+++ neural_mvs_py/train.py
@@ -122,8 +122,8 @@ def run():
     factor_subsample_close_frames=0 #0 means that we use the full resoslution fot he image, anything above 0 means that we will subsample the RGB_closeframes from which we compute the features
     factor_subsample_depth_pred=0
     use_novel_orbit_frame=False #for testing we can either use the frames from the loader or create new ones that orbit aorund the object
-    eval_every_x_epoch=30
-    # eval_every_x_epoch=1
+    # eval_every_x_epoch=30
+    eval_every_x_epoch=1
     do_sfm = False
 
 
@@ -412,8 +412,8 @@ def run():
                                 optimizer=GC_Adam.AdamW( model.parameters(), lr=train_params.lr(), weight_decay=train_params.weight_decay() )
                                 # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=1)
                                 # scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=10000)
-                                # scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True, mode='max', patience=100) #for nerf synthetic when we overfit
-                                scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True, mode='max', patience=1000) #for DTU
+                                scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True, mode='max', patience=100) #for nerf synthetic when we overfit
+                                # scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True, mode='max', patience=1000) #for DTU
                                 # warmup_scheduler = warmup.LinearWarmup(optimizer, warmup_period=3000)
                                 optimizer.zero_grad()
 
